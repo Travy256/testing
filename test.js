90f43fbc2fdafdeae5f51bf7acf4a1bcce563cae
Jacob Philpott <jacob.philpott@gmx.com>
2023-07-30 18:56:14 +0300

    added ball

--- test.js
+++ test.js
@@ -6,10 +6,23 @@ $(document).ready(function() {
   $(".test").click(function() {
     console.log("Hello " + name)
     $("body").css("background-color", "red")
+    $(".test").css("color", "blue")
   })
 
   $("#test2").click(function() {
     console.log("test 2!")
   })
+
+  setInterval(function() {
+
+    setTimeout(function() {
+      $("#ball").animate({top: '400px'}, 1000);
+    }, 1000)
+
+    setTimeout(function() {
+      $("#ball").animate({top: '200px'}, 1000);
+    }, 2000)
+
+  }, 3000)
 
 })
